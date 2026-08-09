5cb922c7cca33c5835ab46383c70a195557a7ffa
AlvajoyAsante <alvajoy123@gmail.com>
2026-08-09 04:01:06 -0400

    Refactor oxy_RenderHmargin to improve visibility check and correct horizontal line positioning

--- oxygen/widget/elements/hmargin.c
+++ oxygen/widget/elements/hmargin.c
@@ -7,10 +7,12 @@
 void oxy_RenderHmargin(struct oxy_widget_t *widget)
 {
 	struct oxy_hmargin_t *hmargin = (struct oxy_hmargin_t *)widget;
+	if (!widget->state.visible)
+		return;
 
 	gfx_SetColor(widget->color.color_a);
 
 	gfx_HorizLine(widget->position.x,
-				  widget->position.y - 1 + (int)(hmargin->alignment == OXY_ALIGN_BOTTOM) * (widget->size.width + 1),
+				  widget->position.y + (hmargin->alignment == OXY_ALIGN_BOTTOM ? widget->size.height - 1 : 0),
 				  widget->size.width);
 }
