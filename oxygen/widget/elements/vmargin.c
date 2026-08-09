f6612cfbb0332eb2a914bc779daf5c8234b3595e
AlvajoyAsante <alvajoy123@gmail.com>
2026-08-09 04:04:42 -0400

    Refactor oxy_RenderVmargin to improve visibility check and correct vertical line positioning

--- oxygen/widget/elements/vmargin.c
+++ oxygen/widget/elements/vmargin.c
@@ -7,10 +7,12 @@
 void oxy_RenderVmargin(struct oxy_widget_t *widget)
 {
 	struct oxy_vmargin_t *vmargin = (struct oxy_vmargin_t *)widget;
+	if (!widget->state.visible)
+		return;
 
 	gfx_SetColor(widget->color.color_a);
 	
-	gfx_VertLine(widget->position.x - 1 + (vmargin->alignment == VMARGIN_ALIGN_RIGHT) * (widget->size.width + 1),
+	gfx_VertLine(widget->position.x + (vmargin->alignment == OXY_ALIGN_RIGHT ? widget->size.width - 1 : 0),
 				 widget->position.y,
 				 widget->size.height);
 }
